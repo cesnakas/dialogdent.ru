012b7e00859a7201126e018d79636b1b2cd64265
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-05-27 16:59:13 +0400

    2.0.002-3

--- local/templates/main/js/main.js
+++ local/templates/main/js/main.js
@@ -1,15 +1,18 @@
 var sliderSwiper = new Swiper('.swiper-slider', {
-  // cssMode: true,
+  
   autoplay: {
     delay: 4500,
     disableOnInteraction: false,
   },
+  
   navigation: {
     prevEl: '.swiper-button-prev',
     nextEl: '.swiper-button-next',
   },
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
+    type: 'progressbar',
   },
   mousewheel: false,
   keyboard: true,
